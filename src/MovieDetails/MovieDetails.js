a64e75dd25b48078090dbf0e5105a33b68793883
Stefan Bloom <stefanjbloom88@gmail.com>
2024-10-19 16:53:53 -0600

    refactor/ MovieDetails to make genre buttons spaced correctly.  CSS styling done"

--- src/MovieDetails/MovieDetails.js
+++ src/MovieDetails/MovieDetails.js
@@ -3,10 +3,14 @@ import './MovieDetails.css';
 function MovieDetails({ movie }) {
   return (
     <section className='MovieDetails'>
-      <img className=''src={movie.backdrop_path} />
-      <h2>{movie.title}</h2>
-      <p>Genres: {movie.genre_ids}</p>
-      <p>{movie.overview}</p>
+      <img className='details-poster'src={movie.backdrop_path} />
+      <h2 className='details-title'>{movie.title}</h2>
+      <div className='details-genres'>
+        {movie.genre_ids.map((genre, index) => (
+          <span key={index} className='genre-button'>{genre}</span>
+        ))}
+        </div>
+      <p className='details-overview'>{movie.overview}</p>
     </section>
   );
 }
